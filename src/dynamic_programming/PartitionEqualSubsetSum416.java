d5cb1b673fa360326d2851ed93cdb00bb9e5c5b6
mainyue <mainyue999@gmail.com>
2024-06-05 00:07:30 +0800

    动态规划

--- src/dynamic_programming/PartitionEqualSubsetSum416.java
+++ src/dynamic_programming/PartitionEqualSubsetSum416.java
@@ -17,11 +17,22 @@ public class PartitionEqualSubsetSum416 {
         sum = sum / 2;
         //可以转换为背包问题：要求符合前n个物品的和 == sum
         boolean[][] dp = new boolean[n + 1][sum + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= sum; j++) {
-                //判断是否符合
-                if(dp[i][])
+                //判断是否能装进背包
+                if (j-nums[i-1] < 0) {
+                    //无法装入背包中，选择不装入背包
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    //能够装入背包中，选择选或者不选当中任意一个符合条件的
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
             }
         }
+        return dp[n][sum];
+
     }
 }
